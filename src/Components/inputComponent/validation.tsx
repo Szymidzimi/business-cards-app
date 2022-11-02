aad236015f8a707bd00f728011104df82dcdc999
Szymidzimi <szymon222333@wp.pl>
2022-11-02 21:12:03 +0100

    add validation register

--- src/Components/inputComponent/validation.tsx
+++ src/Components/inputComponent/validation.tsx
@@ -1,7 +1,3 @@
-export const validateEmail = (email: string) => {
-  const emaailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  return emaailRegex.test(email);
-}
 //validate input not empty
 export const validateNotEmptyInput = (input: string) => {   
   return input.length > 0 && validateSqlInjection(input);
@@ -20,4 +16,27 @@ export const validatePassword = (password: string) => {
 export const validateSqlInjection = (input: string) => {
     const sqlInjectionRegex = /[\s\(\)\'\;\-\=\%]/;
     return !sqlInjectionRegex.test(input);
+}
+//validate to check input for photo to cloudinary
+export const validatePhoto = (photo: string) => {
+    const photoRegex = /^https?:\/\/\S+\.\S+$/;
+    return photoRegex.test(photo);
+}
+//function for validating the maximum number of photos in the input
+export const validateMaxPhotos = (photos: string[]) => {
+    return photos.length <= 5;
+}
+export const validateEmail = (email: string) => {
+  const emaailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  return emaailRegex.test(email);
+}
+//validate to check input for phone number
+export const validatePhone = (phone: string) => {
+    const phoneRegex = /^0\d{9}$/;
+    return phoneRegex.test(phone);
+}
+//validate to check input for website
+export const validateWebsite = (website: string) => {
+    const websiteRegex = /^https?:\/\/\S+\.\S+$/;
+    return websiteRegex.test(website);
 }
--- src/Pages/Register/register.tsx
+++ src/Pages/Register/register.tsx
@@ -1,43 +1,67 @@
 import "./register.css";
-import InputComponent from "../../Components/inputComponent/inputComponent"
+import InputComponent from "../../Components/inputComponent/inputComponent";
 import { useNavigate } from "react-router";
-import React, { useEffect ,useState} from "react";
+import React, { useEffect, useState } from "react";
+import {
+  validateEmail,
+  validateLogin,
+  validateNotEmptyInput,
+} from "../../Components/inputComponent/validation";
 
 const Register: React.FC = () => {
-
-  const navigate=useNavigate();
-  const [errorMessage, setErrorMessage] = useState<string>("")
+  const navigate = useNavigate();
   const [username, setUsername] = useState<string>("");
   const [email, setUserEmail] = useState<string>("");
   const [password, setUserPassword] = useState<string>("");
+  const [usernameValid, setUsernameValid] = useState(true);
+  const [emailValid, setEmailValid] = useState(true);
+  const [passwordValid, setPasswordValid] = useState(true);
+  const [userExists, setUserExists] = useState(false);
+
+  const validateInput = (user: {
+    username: string;
+    email: string;
+    password: string;
+  }) => {
+    setUsernameValid(validateLogin(user.username));
+    setEmailValid(validateEmail(user.email));
+    setPasswordValid(validateNotEmptyInput(user.password));
+    return (
+      validateLogin(user.username) &&
+      validateEmail(user.email) &&
+      validateNotEmptyInput(user.password)
+    );
+  };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    console.log("weszłlo 1");
     e.preventDefault();
 
     const user = {
       username: username,
-      email:email,
+      email: email,
       password: password,
     };
-    try {
-    const response=await fetch("/register", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(user),
-    })
-     const data =await response.json()
-      navigate("/sign");
-      setErrorMessage(data.message);
-      console.log(data.message);
-    } catch (error:any) {
-      console.log(error)
-      setErrorMessage(error)
+    if (validateInput(user)) {
+      try {
+        const response = await fetch("/register", {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(user),
+        });
+        const data = await response.json();
+        if (data.message === "User already exists") {
+          setUserExists(true);
+        } else {
+          setUserExists(false);
+          navigate("/sign");
+        }
+      } catch (error: any) {
+        console.log(error);
+      }
     }
-   
-  }
+  };
   useEffect(() => {
     fetch("/isUserAuth", {
       headers: {
@@ -45,49 +69,61 @@ const Register: React.FC = () => {
       },
     })
       .then((res) => res.json())
-      .then((data) => (data.isLoggedIn ? navigate("/") : null))
-      .catch(err => setErrorMessage(err)) 
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+      .then((data) => (data.isLoggedIn ? navigate("/") : null));
+  }, [navigate]);
 
   return (
     <>
       <div className="register">
         <span className="registerTitle">Register</span>
-        <form className="registerForm"  onSubmit={event=>handleSubmit(event)}>
+        <form
+          className="registerForm"
+          onSubmit={(event) => handleSubmit(event)}
+        >
           <label>Username</label>
           <InputComponent
             type="text"
             value={username}
-            onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setUsername(e.target.value)}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setUsername(e.target.value)
+            }
             placeholder="Enter your username..."
           />
+          {!usernameValid && (
+            <span className="registerError">
+              Username must be at least 3 characters long
+            </span>
+          )}
           <label>Email</label>
           <InputComponent
             type="text"
             value={email}
-            onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setUserEmail(e.target.value)}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setUserEmail(e.target.value)
+            }
             placeholder="Enter your email..."
           />
+          {!emailValid && (
+            <span className="registerError">Email is not valid</span>
+          )}
           <label>Password</label>
           <InputComponent
             type="password"
             value={password}
-            onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setUserPassword(e.target.value)}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setUserPassword(e.target.value)
+            }
             placeholder="Enter your password..."
           />
+          {!passwordValid && (
+            <span className="registerError">
+              Password must be at least 6 characters long
+            </span>
+          )}
+          {userExists && <p className="error">User exist</p>}
+
           <button className="registerButton">Register</button>
         </form>
-
-      
-
-      <div className="social-block">
-        <p>{errorMessage}</p>
-        <a className="btn btn-block" href="/auth/google" role="button">
-          <i className="fab fa-google"></i>
-          Sign Up with Google
-        </a>
-      </div>
       </div>
     </>
   );
