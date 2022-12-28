bd5f32438e542bf84b45725df62baa1032c3d91d
Szymidzimi <szymon222333@wp.pl>
2022-12-28 13:10:53 +0100

    profile name

--- src/Components/navBar/navbar.tsx
+++ src/Components/navBar/navbar.tsx
@@ -1,28 +1,39 @@
 import "./navbar.css";
 import React, { useState } from "react"
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import {faBars,faTimes} from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useNavigate } from "react-router";
+import {FaUserCircle} from "react-icons/fa"
+import { getUserData } from "../../config/decodeUser";
 
 const Navbar: React.FC = () => {
+  const navigate = useNavigate();
+  // const [userDataToken, setUserDataToken] = useState<TokenUserData | null| undefined>();
 
-  const navigate=useNavigate();
-  async function logout() {
-    localStorage.removeItem("token")
-    navigate("/");
-}
 
   const [Mobile, setMobile] = useState(false)
+
+  // const getUserData = (): TokenUserData | null => {
+  //   const token = localStorage.getItem("token")?.replace("Bearer ", "");
+  //   if (token) {
+  //     const user: TokenUserData = jwtDecode(token);
+  //     return user
+  //   //   return { id: user.id, username: user.userName};
+  //   }
+  //   return null;
+  // };
+
   return (
     <header>
     <nav>
-      <h3 className="logo">AgroHand</h3>
+    <Link to='/'><h3 className="logo">AgroHand</h3></Link>
        <ul className={Mobile ? "nav-links-mobile" : "nav-links"} onClick={() => setMobile(false)}>
           <Link to='/'><li className="topListItem">HOME</li></Link>
           <Link to='/search'><li className="topListItem">SEARCH ENTERPRISE</li></Link>
           <Link to='/add'><li className="topListItem">ADD ENTERPRISE</li></Link>
-          {(localStorage.getItem('token'))?<li><button className="logoutButton" onClick={logout}>LOGOUT</button></li>:<Link to='/sign'><li className="topListItem">SIGN IN</li></Link>}
+          {/* {(localStorage.getItem('token'))?<Link to='/profile'><li className="topListUser"><FaUserCircle size={40}></FaUserCircle></li></Link>:<Link to='/sign'><li className="topListItem">SIGN IN</li></Link>}*/}
+          {(localStorage.getItem('token'))?<Link to='/profile'><li><div className="topListUser"><FaUserCircle size={40}></FaUserCircle><div className="user-name">{getUserData()?.username}</div></div></li></Link>:<Link to='/sign'><li className="topListItem">SIGN IN</li></Link>} 
+
         </ul>
         <button className='mobile-menu-icon' onClick={() => setMobile(!Mobile)}>
           {Mobile ? <FontAwesomeIcon icon={faTimes} /> : <FontAwesomeIcon icon={faBars} />}
